b644b521d489a7fcb3e882f1713d82016bca33fb
luciagf23 <luciagf23@educastur.es>
2025-10-22 13:55:28 +0200

    Main

--- TAREA1_luciagarcia/src/main/java/com/lucigf/modelo/Sesion.java
+++ TAREA1_luciagarcia/src/main/java/com/lucigf/modelo/Sesion.java
@@ -15,279 +15,109 @@ import java.util.regex.Pattern;
 
 public class Sesion {
 
+	private Long id;
 	private String nombreUsuario;
+	private String password;
 	private Perfil perfilActual;
 
 	
 	
-	public Sesion(String nombreUsuario, Perfil perfilActual) {
+
+	public Sesion(Long id, String nombreUsuario, String password, Perfil perfilActual) {
+		super();
+		this.id = id;
 		this.nombreUsuario = nombreUsuario;
+		this.password = password;
 		this.perfilActual = perfilActual;
 	}
 
 
 
 
-	public boolean login(String usuario, String contrasenia) {
-		
-		if(perfilActual != Perfil.INVITADO) {
-			System.out.println("Ya hay una sesion activa.");
-			return false;
-		}
-		
-		 if (usuario.isEmpty() || contrasenia.isEmpty()) {
-	            System.out.println("Usuario o contraseña vacíos");
-	            return false;
-	        }
-		/*
-		if("admin".equals(usuario) && "admin".equals(contrasenia)) {
-			perfilActual=Perfil.ADMIN;
-			nombreUsuario="Admin";
-			return true;
-		}
-		*/
-		
-		try(BufferedReader br=new BufferedReader(new FileReader("credenciales.txt"))){
-			
-			String linea;
-			while((linea=br.readLine())!=null) {
-				
-				String[]datos=linea.split("\\|");
-				if(datos.length>=7 && datos.equals(usuario) && datos.equals(contrasenia)) {
-					nombreUsuario=datos[1];
-					perfilActual=Perfil.valueOf(datos[6]);
-					System.out.println("Login correcto. Bienvenido, " +datos[4]+ "("+perfilActual+ ")");
-					return true;
-				}
-					
-			}
-				
-				
-			}catch(IOException e) {
-				System.out.println("Error al leer credencieles.txt " +e.getMessage());
-			}
-			
-			System.out.println("Credenciales incorrectas");
-			return false;
-		}
-		
-	
-	
-		public void logout() {
-			if(perfilActual==Perfil.INVITADO) {
-				System.out.println("No hay sesion activa para cerrar");
-				return;
-			}
-			
-			System.out.println("Sesión cerrada " +nombreUsuario);
-			perfilActual=Perfil.INVITADO;
-			nombreUsuario=null;
-		}
-		
-	
-	
-	
-	
-	public void registrarPersona() throws FileNotFoundException, IOException {
-		Scanner teclado=new Scanner(System.in);
-		if(perfilActual!=Perfil.ADMIN) {
-			System.out.println("Acesso denegado, solo el ADMIN puede registrar personas");
-			return;
-		}
-		
-		System.out.println("== Registrar nueva persona ==");
-		System.out.println("Nombre: ");
-		String nombre=teclado.next().trim();
-		System.out.println("Email: ");
-		String email=teclado.next().trim();
-		System.out.println("Nacionalidad: ");
-		String nacionalidad=teclado.next().trim();
-		System.out.println("Usuario: ");
-		String usuario=teclado.next().trim();
-		System.out.println("Contraseña: ");
-		String contrasenia=teclado.next().trim();
-		System.out.println("Perfil: COORDINACION O ARTISTA");
-		String tipoPerfil=teclado.nextLine().trim().toUpperCase();
-		
-		
-		boolean esSenior=false;
-		String fechaSenior="";
-		String apodo="";
-		String especialidades="";
-		
-	
-		
-		if(tipoPerfil.equals("COORDINACION")) {
-			System.out.println("¿Es senior? (S/N): ");
-			String respuesta=teclado.nextLine().trim().toUpperCase();
-			if(respuesta.equals("S")){
-					esSenior=true;
-					System.out.println("Fecha desde que es senior (DD/MM/AAAA): ");
-					fechaSenior=teclado.nextLine().trim();
-			}
-			
-		}else {
-			System.out.println("¿Tiene apodo artístico? (S/N): ");
-			String respuesta=teclado.nextLine().trim().toUpperCase();
-			if(respuesta.equals("S")) {
-				System.out.println("Apodo: ");
-				apodo=teclado.nextLine().trim();
-			}
-			
-			
-			System.out.println("Especialidades disponibles: " +Arrays.toString(Especialidad.values()));
-			
-			boolean ok = false;
-		    Set<Especialidad> especialidadesSeleccionadas = EnumSet.noneOf(Especialidad.class);
+	public Long getId() {
+		return id;
+	}
 
-		    while (!ok) {
-		        System.out.print("Introduce las especialidades separadas por comas: ");
-		        String entrada = teclado.nextLine().trim().toUpperCase();
 
-		        if (entrada.isEmpty()) {
-		            System.out.println("Debes introducir al menos una especialidad");
-		            continue;
-		        }
-		        
-		        String[]partes=entrada.split(",");
-		        ok=true;
-		        
-		        
-		        for(String p:partes) {
-		        	try {
-		        		Especialidad esp=Especialidad.valueOf(p.trim());
-		        	}catch(IllegalArgumentException e) {
-		        		System.out.println("No es una especialidad válida");
-		        		ok=false;
-		        		break;
-		        		
-		        	}
-		        }
-		      }
-		    especialidades = especialidadesSeleccionadas.toString()
-		            .replace("[", "")
-		            .replace("]", "")
-		            .replace(" ", "");
-			
-		}
-		
-		
-		//Credenciales
-		System.out.println("Nombre de usuario: ");
-		usuario=teclado.nextLine().trim().toLowerCase();
-		if(!Pattern.matches("[a-z] {3,}", usuario)) {
-			System.out.println("La contraseña debe tener al menos 3 caracteres y no contener espacios");
-			return;
-		}
-		
-		
-		
-		//duplicados
-		try(BufferedReader br=new BufferedReader(new FileReader("credenciales.txt"))){
-			String linea;
-			while((linea=br.readLine())!=null) {
-				String[]datos=linea.split("\\|");
-				if(datos.length>=7) {
-					if(datos[1].equals(usuario)) {
-						System.out.println("Ya existe usuario con ese nombre");
-						return;
-					}
-					if(datos[3].equalsIgnoreCase(email)) {
-						System.out.println("Ya existe una persona registrada con ese email");
-						return;
-					}
-				}
-			}
-		}catch(FileNotFoundException e){
-			
-		}catch(IOException e) {
-			System.out.println("Error leyendo credenciales.txt: " +e.getMessage());
-		}
-		
-		
-		//Calcular id
-		
-		
-		int idpersona=contarLineasFichero("credenciales.txt")+1;
-		
-		//Escribir en el fichero
-		try(BufferedWriter bw=new BufferedWriter(new FileWriter("credenciales.txt",true))){
-			bw.write((idpersona++)+ "|" + usuario+ "|" + contrasenia+ "|" +email+ "|" +nacionalidad+ "|" +tipoPerfil);
-			bw.newLine();
-			System.out.println("Persona registrada correctamente");
-		}catch(FileNotFoundException e) {
-			System.out.println(e.getMessage());
-			
-		}catch (IOException e) {
-			System.out.println("Error escribiendo en credenciales.txt" +e.getMessage());
-		}
-		
-		
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+
+
+	public String getNombreUsuario() {
+		return nombreUsuario;
+	}
+
+
+
+
+	public void setNombreUsuario(String nombreUsuario) {
+		this.nombreUsuario = nombreUsuario;
+	}
+
+
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+
+
+	public Perfil getPerfilActual() {
+		return perfilActual;
+	}
+
+
+
+
+	public void setPerfilActual(Perfil perfilActual) {
+		this.perfilActual = perfilActual;
+	}
+
+
+
+
+	@Override
+	public String toString() {
+		return "Sesion [id=" + id + ", nombreUsuario=" + nombreUsuario + ", password=" + password + ", perfilActual="
+				+ perfilActual + "]";
+	}
+
+
+
+
+	public void mostrarMenu() {
+		// TODO Auto-generated method stub
 		
 	}
-	
-	
-	private int contarLineasFichero(String nombreFichero) {
-		int contador=0;
-		try(BufferedReader br=new BufferedReader(new FileReader(nombreFichero))){
-			while(br.readLine()!=null)
-				contador++;
-		}catch(IOException e) {
-			
-		}
-		return contador;
+
+
+
+
+	public void registrarPersona() {
+		// TODO Auto-generated method stub
+		
 	}
-	
-	
-	
-	// Menu dependiendo del perfil
 
-		public void mostrarMenu() {
-			if(perfilActual==null) {
-				
-				System.out.println("== Menú Invitado ==" );
-				System.out.println("1. Ver espectáculos");
-				System.out.println("0. Salir");
-			}else {
-				switch(perfilActual) {
-				case ADMIN:
-					System.out.println("== Menú Administrador ==");
-					System.out.println("1. Registrar nueva persona");
-					System.out.println("2. Gestionar credenciales");
-					System.out.println("3. Gestionar espectáculos");
-					System.out.println("4. Ver todos los datos del circo");
-					System.out.println("0. Cerrar sesión");
-					break;
-					
-				case COORDINACION:
-					System.out.println("== Menú Coordinación ==");
-					System.out.println("1. Crear o modificar espectáculos");
-					System.out.println("2. Gestionar números");
-					System.out.println("3. Asignar artistas a números");
-					System.out.println("4. Ver información completa de espectáculos");
-					System.out.println("0. Cerrar sesión");
-					break;
-					
-				case ARTISTA:
-					System.out.println("== Menú Artista ==");
-					System.out.println("1. Ver mi ficha personal");
-					System.out.println("2. Ver mis espectáculos y números");
-					System.out.println("0. Cerrar sesión");
-					break;
-				}
-			}
-		}
+
+
 	
 
 	
-	 // Getters
-    public Perfil getPerfilActual() {
-        return perfilActual;
-    }
 
-    public String getNombreUsuario() {
-        return nombreUsuario;
-    }
 	
 
 }
--- TAREA1_luciagarcia/src/main/java/principal/Main.java
+++ TAREA1_luciagarcia/src/main/java/principal/Main.java
@@ -1,23 +1,34 @@
 package principal;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.nio.channels.NonWritableChannelException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.EnumSet;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.regex.Pattern;
 
 import com.lucigf.modelo.Artista;
 import com.lucigf.modelo.Coordinacion;
 import com.lucigf.modelo.Credenciales;
+import com.lucigf.modelo.Especialidad;
 import com.lucigf.modelo.Espectaculo;
 import com.lucigf.modelo.Perfil;
 import com.lucigf.modelo.Sesion;
 
 public class Main {
 
-	private static Sesion sesionActual=new Sesion(null, Perfil.INVITADO);
+	private static String nombreUsuario=null;
+	private static Perfil perfilActual = Perfil.INVITADO;
+	private static Sesion sesionActual=new Sesion(null,Perfil.INVITADO);
 	private static Scanner teclado=new Scanner(System.in);
 	
 	//CU1: Ver espectáculos básicos
@@ -43,18 +54,37 @@ public class Main {
 	
 		//CU2: Iniciar sesión
 		
-		public static void iniciarSesion() {
+		public static boolean iniciarSesion(String usuario,String contrasenia){
 			
-			if(sesionActual.getPerfilActual()!=Perfil.INVITADO) {
+			if(perfilActual !=Perfil.INVITADO) {
 				System.out.println("Ya hay una sesión activa");
-				return;
+				return false;
 			}
 		
-			System.out.println("== INICIO DE SESIÓN ==");
-			System.out.println("Usuario: ");
-			String usuario=teclado.nextLine();
-			System.out.println("Contraseña: ");
-			String password=teclado.nextLine();
+			if (usuario.isEmpty() || contrasenia.isEmpty()) {
+	            System.out.println("Usuario o contraseña vacíos");
+	            return false;
+	        }
+			
+			try(BufferedReader br=new BufferedReader(new FileReader("credenciales.txt"))){
+				
+				String linea;
+				while((linea=br.readLine())!=null) {
+					
+					String[]datos=linea.split("\\|");
+					if(datos.length>=7 && datos.equals(usuario) && datos.equals(contrasenia)) {
+						nombreUsuario=datos[1];
+						perfilActual=Perfil.valueOf(datos[6]);
+						System.out.println("Login correcto. Bienvenido, " +datos[4]+ "("+perfilActual+ ")");
+						return true;
+					}
+				}
+			}catch(IOException e) {
+				System.out.println("Error al leer credencieles.txt " +e.getMessage());
+			}
+			
+			System.out.println("Credenciales incorrectas");
+			return false;
 			
 			
 		}
@@ -62,18 +92,186 @@ public class Main {
 		//Cerrar sesión 
 		
 		public static void cerrarSesion() {
-			if(sesionActual.getPerfilActual()== Perfil.INVITADO) {
+			if(perfilActual== Perfil.INVITADO) {
 				System.out.println("No hay ninguna sesion activa");
-				
-			}else{
-				sesionActual.logout();
+				return;
 			}
+			System.out.println("Sesión cerrada " +nombreUsuario);
+			perfilActual=Perfil.INVITADO;
+			nombreUsuario=null;
 			
 		}
 		
+		public static void registrarPersona() throws IOException {
+	        Scanner teclado = new Scanner(System.in);
+	        if (perfilActual != Perfil.ADMIN) {
+	            System.out.println("Acceso denegado, solo el ADMIN puede registrar personas");
+	            return;
+	        }
+
+	        System.out.println("== Registrar nueva persona ==");
+	        System.out.print("Nombre: ");
+	        String nombre = teclado.nextLine().trim();
+	        System.out.print("Email: ");
+	        String email = teclado.nextLine().trim();
+	        System.out.print("Nacionalidad: ");
+	        String nacionalidad = teclado.nextLine().trim();
+	        System.out.print("Usuario: ");
+	        String usuario = teclado.nextLine().trim();
+	        System.out.print("Contraseña: ");
+	        String contrasenia = teclado.nextLine().trim();
+	        System.out.print("Perfil (COORDINACION/ARTISTA): ");
+	        String tipoPerfil = teclado.nextLine().trim().toUpperCase();
+
+	        boolean esSenior = false;
+	        String fechaSenior = "";
+	        String apodo = "";
+	        String especialidades = "";
+
+	        if (tipoPerfil.equals("COORDINACION")) {
+	            System.out.print("¿Es senior? (S/N): ");
+	            String respuesta = teclado.nextLine().trim().toUpperCase();
+	            if (respuesta.equals("S")) {
+	                esSenior = true;
+	                System.out.print("Fecha desde que es senior (DD/MM/AAAA): ");
+	                fechaSenior = teclado.nextLine().trim();
+	            }
+	        } else {
+	            System.out.print("¿Tiene apodo artístico? (S/N): ");
+	            String respuesta = teclado.nextLine().trim().toUpperCase();
+	            if (respuesta.equals("S")) {
+	                System.out.print("Apodo: ");
+	                apodo = teclado.nextLine().trim();
+	            }
+
+	            System.out.println("Especialidades disponibles: " + Arrays.toString(Especialidad.values()));
+	            boolean ok = false;
+	            Set<Especialidad> especialidadesSeleccionadas = EnumSet.noneOf(Especialidad.class);
+
+	            while (!ok) {
+	                System.out.print("Introduce las especialidades separadas por comas: ");
+	                String entrada = teclado.nextLine().trim().toUpperCase();
+
+	                if (entrada.isEmpty()) {
+	                    System.out.println("Debes introducir al menos una especialidad");
+	                    continue;
+	                }
+
+	                String[] partes = entrada.split(",");
+	                ok = true;
+
+	                for (String p : partes) {
+	                    try {
+	                        Especialidad esp = Especialidad.valueOf(p.trim());
+	                        especialidadesSeleccionadas.add(esp);
+	                    } catch (IllegalArgumentException e) {
+	                        System.out.println("No es una especialidad válida");
+	                        ok = false;
+	                        break;
+	                    }
+	                }
+	            }
+	            especialidades = especialidadesSeleccionadas.toString()
+	            		.replace("[", "")
+	            		.replace("]", "")
+	            		.replace(" ", "");
+	        }
+	        
+	        
+	      //Credenciales
+			System.out.println("Nombre de usuario: ");
+			usuario=teclado.nextLine().trim().toLowerCase();
+			if(!Pattern.matches("[a-z] {3,}", usuario)) {
+				System.out.println("La contraseña debe tener al menos 3 caracteres y no contener espacios");
+				return;
+			}
+			
+			//duplicados
+			try(BufferedReader br=new BufferedReader(new FileReader("credenciales.txt"))){
+				String linea;
+				while((linea=br.readLine())!=null) {
+					String[]datos=linea.split("\\|");
+					if(datos.length>=7) {
+						if(datos[1].equals(usuario)) {
+							System.out.println("Ya existe usuario con ese nombre");
+							return;
+						}
+						if(datos[3].equalsIgnoreCase(email)) {
+							System.out.println("Ya existe una persona registrada con ese email");
+							return;
+						}
+					}
+				}
+			}catch(FileNotFoundException e){
+				
+			}catch(IOException e) {
+				System.out.println("Error leyendo credenciales.txt: " +e.getMessage());
+			}
+			
+	        
+	        
+	        int idpersona = contarLineasFichero("credenciales.txt") + 1;
 		
+	        //Escribir en el fichero
+	        
+	        try (BufferedWriter bw = new BufferedWriter(new FileWriter("credenciales.txt", true))) {
+	            bw.write(idpersona + "|" + usuario + "|" + contrasenia + "|" + email + "|" + nacionalidad + "|" + tipoPerfil + "|" + perfilActual);
+	            bw.newLine();
+	            System.out.println("Persona registrada correctamente");
+	        } catch (IOException e) {
+	            System.out.println("Error escribiendo en credenciales.txt: " + e.getMessage());
+	        }
+	    }
+		
+		
+		private int contarLineasFichero(String nombreFichero) {
+			int contador=0;
+			try(BufferedReader br=new BufferedReader(new FileReader(nombreFichero))){
+				while(br.readLine()!=null)
+					contador++;
+			}catch(IOException e) {
+				
+			}
+			return contador;
+		}
 	
-	
+		// Menu dependiendo del perfil
+
+				public void mostrarMenu() {
+					if(perfilActual==null) {
+						
+						System.out.println("== Menú Invitado ==" );
+						System.out.println("1. Ver espectáculos");
+						System.out.println("0. Salir");
+					}else {
+						switch(perfilActual) {
+						case ADMIN:
+							System.out.println("== Menú Administrador ==");
+							System.out.println("1. Registrar nueva persona");
+							System.out.println("2. Gestionar credenciales");
+							System.out.println("3. Gestionar espectáculos");
+							System.out.println("4. Ver todos los datos del circo");
+							System.out.println("0. Cerrar sesión");
+							break;
+							
+						case COORDINACION:
+							System.out.println("== Menú Coordinación ==");
+							System.out.println("1. Crear o modificar espectáculos");
+							System.out.println("2. Gestionar números");
+							System.out.println("3. Asignar artistas a números");
+							System.out.println("4. Ver información completa de espectáculos");
+							System.out.println("0. Cerrar sesión");
+							break;
+							
+						case ARTISTA:
+							System.out.println("== Menú Artista ==");
+							System.out.println("1. Ver mi ficha personal");
+							System.out.println("2. Ver mis espectáculos y números");
+							System.out.println("0. Cerrar sesión");
+							break;
+						}
+					}
+				}
 		
 	
 	
